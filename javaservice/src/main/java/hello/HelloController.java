49935424e2e7c6f516f2bda89b5022349d13f263
Devinwhit <Devinwhit@gmail.com>
2019-09-01 10:12:24 -0400

    Adding CORS support & testing rest controller with angular frontend

--- javaservice/src/main/java/hello/HelloController.java
+++ javaservice/src/main/java/hello/HelloController.java
@@ -1,14 +1,28 @@
 package hello;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@CrossOrigin(origins = "*")
 @RestController
 public class HelloController {
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot! - testing pushed change and github webhook...once again!s";
+    }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String,Object> model = new HashMap<String,Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
     }
 
 }
