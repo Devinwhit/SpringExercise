dd8f90204177e3833ef59bd9e6db671e0234248b
Devinwhit <Devinwhit@gmail.com>
2019-09-15 14:09:33 -0400

    Adding more pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,23 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import {MatToolbarModule,  MatIconModule, MatSidenavModule, MatListModule, MatButtonModule} from '@angular/material';
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatListModule,
+  MatButtonModule,
+  MatCardModule,
+  MatExpansionModule
+} from '@angular/material';
 import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { EducationComponent } from './resume/education/education.component';
+import { WorkExperienceComponent } from './resume/work-experience/work-experience.component';
+import { SkillsComponent } from './resume/skills/skills.component';
 
 
 @NgModule({
@@ -16,7 +28,11 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     PageNotFoundComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ResumeComponent,
+    EducationComponent,
+    WorkExperienceComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +44,9 @@ import { HomeComponent } from './home/home.component';
     MatButtonModule,
     MatIconModule,
     LayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  page = {
+    title: 'Home',
+    subtitle: 'Welcome Home!',
+    content: 'Some home content.',
+    image: 'assets/images/Okemo.jpg'
+  };
 
   constructor() { }
 
