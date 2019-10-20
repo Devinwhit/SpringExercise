1a3ae0a916aec5a4cd4b4729598d97d8eed13bc9
Devinwhit <Devinwhit@gmail.com>
2019-10-20 16:20:17 -0400

    Working on school info

--- frontend/src/app/resume/education/education.component.ts
+++ frontend/src/app/resume/education/education.component.ts
@@ -8,19 +8,6 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      let x = 2;
-      if (matches) {
-        x = 1;
-      }
-      return [
-        { title: 'Udacity', content: 'Won scholarship from Google to earn an Android Developer nano-degree', cols: x, rows: 1 },
-        { title: 'UMass Lowell', content: 'Earned second Bachelors, in Science, in 2017', cols: x, rows: 1 }
-      ];
-    })
-  );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor() {}
 }
