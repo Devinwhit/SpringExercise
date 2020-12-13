ad7f4ec5d98c15f64db06ce552eecbb98e439cea
Devinwhit <Devinwhit@gmail.com>
2020-12-13 17:23:42 -0500

    finalizing k8s setup
    Pipeline is finally working!

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -9,7 +9,7 @@ export class HomeComponent implements OnInit {
   page = {
     title: 'Home',
     subtitle: 'Welcome Home!',
-    content: 'Site is currently being developed. Updates to pipeline 4.4.2020 - additional updates with successful pipeline!',
+    content: 'Site is always changing, now being hosted on kubernetes.',
     image: 'assets/images/Okemo.jpg'
   };
 
